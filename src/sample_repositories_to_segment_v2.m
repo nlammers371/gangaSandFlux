3ea9e1a6e7581dcc72e5338e399be82f17a4033d
nlammers371 <nlammers@berkeley.edu>
2022-02-28 16:53:40 -0800

    Old changes

--- src/sample_repositories_to_segment_v2.m
+++ src/sample_repositories_to_segment_v2.m
@@ -115,10 +115,11 @@ region_date_diffs(region_date_diffs(:,1)~=0,2) = Inf;
 % calculate average x and y positions
 x_region_mean_vec = grpstats(x_mean_vec,rd_map_vec,'mean');
 y_region_mean_vec = grpstats(y_mean_vec,rd_map_vec,'mean');
+[~,region_pca_array] = pca([x_mean_vec y_mean_vec]);
 
 % Identify duples with sufficiently close time points
 max_dt = 15; % maximum temporal separation
-n_repos_per_region = 10;
+n_repos_per_region = 15;
 nBins = 5;
 total_samples = 1e3;
 
@@ -138,14 +139,17 @@ dt_vec_filtered = region_date_diffs(dt_filter_vec,2);
 linIndex = sub2ind(size(count_array),xID,yID);
 
 % generate weight vector 
-sample_weight_vec = 1./dt_vec_filtered.^2 .* 1./count_array(linIndex);
+sample_weight_vec = (1./dt_vec_filtered).^2;%.* 1./count_array(linIndex);
 
 %% perform actual sampling
 sampled_flags = false(size(sample_weight_vec));
 sample_counter = 1;
+iter_counter = 1;
 sample_struct = struct;
-rng(421);
-while sample_counter <= total_samples && ~all(sampled_flags)
+rng(147);
+
+while ~all(sampled_flags) && sample_counter < total_samples
+  
     % randomly draw region/date id
     samp_options = find(~sampled_flags);
     if length(samp_options) > 1
@@ -153,6 +157,7 @@ while sample_counter <= total_samples && ~all(sampled_flags)
     else
         samp_id = samp_options;
     end
+    
     % update tracking vector
     sampled_flags(samp_id) = true;
     
@@ -161,105 +166,140 @@ while sample_counter <= total_samples && ~all(sampled_flags)
     date_id = region_date_array(dt_filter_vec(samp_id),2);
     date_id2 = region_date_array(dt_filter_vec(samp_id)+1,2);
     repo_ids = find(region_id_vec == region_id & ...
-                    dateNumVec == date_id);
+                       dateNumVec == date_id);
                   
     % randomly select repos to use
-    repo_rank_list = randsample(1:length(repo_ids),min([length(repo_ids),2*n_repos_per_region]),false);
-    ranked_repo_id_list = repo_ids(repo_rank_list);
-    primary_flag = false(size(ranked_repo_id_list));
-    primary_flag(1:min([n_repos_per_region length(primary_flag)])) = true;
+    repo_rank_list = randsample(1:length(repo_ids),min([length(repo_ids) 2*n_repos_per_region]),false);
+    repo_id_list = repo_ids(repo_rank_list);
     
-    % add to structure
-    for i = 1:length(primary_flag)
-        sample_struct(sample_counter).region_name = region_id_index{region_id};
-        sample_struct(sample_counter).date_1 = dateStringCell{ranked_repo_id_list(i)};
-        sample_struct(sample_counter).date_2 = datestr(date_id2,'mmddyyyy');
-        sample_struct(sample_counter).repo_name_raw = NameIndexRaw1(ranked_repo_id_list(i));
-        sample_struct(sample_counter).repo_name_clean = NameIndexClean(ranked_repo_id_list(i));
-        sample_struct(sample_counter).primary_flag = primary_flag(i); 
+    for r = 1:length(repo_rank_list)
+      
+        repo_id = repo_id_list(r);
         
-        sample_counter = sample_counter + 1*primary_flag(i);
+        % add info
+        sample_struct(iter_counter).region_name = region_id_index{region_id};
+        sample_struct(iter_counter).region_id = region_id;
+        sample_struct(iter_counter).repo_rank_vec = r;
+        sample_struct(iter_counter).primary_sample_flag = 1*(r < n_repos_per_region); 
+        sample_struct(iter_counter).date_1 = dateStringCell{repo_id};
+        sample_struct(iter_counter).date_2 = datestr(date_id2,'mmddyyyy');
+        sample_struct(iter_counter).repo_name_raw = NameIndexRaw1(repo_id);
+        sample_struct(iter_counter).repo_name_clean = NameIndexClean(repo_id);
+        sample_struct(iter_counter).mean_x_pos = x_mean_vec(repo_id);
+        sample_struct(iter_counter).mean_y_pos = x_mean_vec(repo_id);
+        sample_struct(iter_counter).mean_pca_pos = region_pca_array(repo_id);                
+        sample_struct(iter_counter).date_1_num = dateNumVec(repo_id);
+
+        iter_counter = iter_counter + 1;
+        sample_counter = sample_counter + 1*(r < n_repos_per_region);
     end
 end  
-%% Rearrange vectors and identify regions with multiple entries
-[~, si] = sortrows([master_id_vec dateNumVec]);
-x_mean_vec_sorted = x_mean_vec(si);
-y_mean_vec_sorted = y_mean_vec(si);
-[~, dist_score_array] = pca([x_mean_vec_sorted y_mean_vec_sorted]);
 
-repoCellSorted = repoCell(si);
-regionCellSorted = regionCell(si);
-master_id_vec_sorted = master_id_vec(si);
-subIDVec = subIDVec(si);
-dateNumVec = dateNumVec(si);
-NameIndexCleanSorted = NameIndexClean(si);
-NameIndexRawSorted = NameIndexRaw1(si);
-dateStringCellSorted = dateStringCell(si);
+% Convert to table
+sample_table = struct2table(sample_struct);
+% reorder by region-date-rank order
+sample_table = sortrows(sample_table,[2 12 3]);
 
-% Get counts by repo
-[iu_rep, map_to, map_from] = unique(master_id_vec_sorted);
+writetable(sample_table,[OutDir 'repo_sample_list.csv'])
+%% Look at distribution of selected regions
+samp_pos_vec = [sample_struct.mean_pca_pos]/1e4;
+samp_date_vec = [sample_struct.date_1_num]/1e5;
+samp_prim_vec = [sample_struct.primary_sample_flag];
 
-rep_count_vec_long = NaN(size(map_from));
-rep_count_vec = NaN(size(iu_rep));
-date_count_vec_long = NaN(size(map_from));
-date_diff_vec = NaN(size(iu_rep));
+close all
+cmap = brewermap(9,'Set2');
+samp_fig = figure;
+hold on
+scatter(region_pca_array(:,1)/1e4,dateNumVec/1e5,'MarkerFaceColor',cmap(8,:),'MarkerFaceAlpha',0.2,'MarkerEdgeAlpha',0.2,'MarkerEdgeColor','k')
+% scatter(samp_pos_vec,samp_date_vec,'MarkerFaceColor',cmap(3,:),'MarkerFaceAlpha',0.2,'MarkerEdgeAlpha',0.2,'MarkerEdgeColor','k')
+scatter(samp_pos_vec(samp_prim_vec==1),samp_date_vec(samp_prim_vec==1),'d','MarkerFaceColor',cmap(2,:),'MarkerFaceAlpha',0.3,'MarkerEdgeAlpha',0.2,'MarkerEdgeColor','k')
 
-for i = 1:length(iu_rep)
-  
-    % number of occurrences
-    rep_count_vec_long(map_from==i) = sum(map_from==i);
-    rep_count_vec(i) = sum(map_from==i);
-    
-    if sum(map_from==i) > 1
-        date_diff_vec(i) = median(diff(dateNumVec(map_from==i)));
-        date_count_vec_long(map_from==i) = median(diff(dateNumVec(map_from==i)));
-    end
-end
+grid on
+xlabel('repo position (au)')
+xlabel('repo date (au)')
+set(gca,'Fontsize',14)
+legend('all repositories','sampled repositories','Location','southeast')
 
-%% Get frequency counts for distribution in space and time
-min_reps = 2; % minimum number of times a repo can appear in the dataset
-max_med_date_dist = 180; % maximum median distance between consecutive observations
-nBins = 10;
+saveas(samp_fig,'../fig/sample_set_dispersion.png')
 
-% apply filter
-date_rep_filter = find(rep_count_vec >= min_reps & date_diff_vec <= max_med_date_dist);
-
-% get empircal distribution of counts
-dateNumVecTrunc = dateNumVec(map_to);
-posVecTrunc = dist_score_array(map_to);
-[count_array,~,~,xID,yID] = histcounts2(dateNumVecTrunc,posVecTrunc,nBins);
-
-% draw random sample weighted by empirical counts
-linIndex = sub2ind(size(count_array),xID,yID);
-rng(421);
-region_index_list = randsample(date_rep_filter,1e4*length(date_rep_filter),true,1./count_array(linIndex(date_rep_filter)));
-
-% enforce uniqueness
-region_index_list_u = unique(region_index_list,'stable');
-region_id_list_u = iu_rep(region_index_list_u);
-
-% map back to original dimensions
-% region_name_cell = {}; 
-remapping_vec = [];
-for i = 1:length(region_id_list_u)
-    remapping_vec = [remapping_vec find(map_from==region_id_list_u(i))'];
-%     region_name_cell = [region_name_cell{:} NameIndexCleanSorted(map_from==region_id_list_u(i))'];
-end    
-
-%% initialize structure
-region_sampling_table = struct;
-
-master_id_vec_orig = master_id_vec_sorted(remapping_vec);
-[~,~,region_sampling_table.sample_order_vec] = unique(master_id_vec_orig,'stable');
-
-region_sampling_table.region_name_clean = NameIndexCleanSorted(remapping_vec);
-region_sampling_table.region_name_orig = NameIndexRawSorted(remapping_vec);
-% region_sampling_table.date_num = dateNumVec(remapping_vec);
-region_sampling_table.date_string = dateStringCell(remapping_vec);
-
-
-region_sampling_table = struct2table(region_sampling_table);
-
-% save
-writetable(region_sampling_table,[OutDir 'region_sampling_list.csv']);
-
+% [~, si] = sortrows([master_id_vec dateNumVec]);
+% x_mean_vec_sorted = x_mean_vec(si);
+% y_mean_vec_sorted = y_mean_vec(si);
+% [~, dist_score_array] = pca([x_mean_vec_sorted y_mean_vec_sorted]);
+% 
+% repoCellSorted = repoCell(si);
+% regionCellSorted = regionCell(si);
+% master_id_vec_sorted = master_id_vec(si);
+% subIDVec = subIDVec(si);
+% dateNumVec = dateNumVec(si);
+% NameIndexCleanSorted = NameIndexClean(si);
+% NameIndexRawSorted = NameIndexRaw1(si);
+% dateStringCellSorted = dateStringCell(si);
+% 
+% % Get counts by repo
+% [iu_rep, map_to, map_from] = unique(master_id_vec_sorted);
+% 
+% rep_count_vec_long = NaN(size(map_from));
+% rep_count_vec = NaN(size(iu_rep));
+% date_count_vec_long = NaN(size(map_from));
+% date_diff_vec = NaN(size(iu_rep));
+% 
+% for i = 1:length(iu_rep)
+%   
+%     % number of occurrences
+%     rep_count_vec_long(map_from==i) = sum(map_from==i);
+%     rep_count_vec(i) = sum(map_from==i);
+%     
+%     if sum(map_from==i) > 1
+%         date_diff_vec(i) = median(diff(dateNumVec(map_from==i)));
+%         date_count_vec_long(map_from==i) = median(diff(dateNumVec(map_from==i)));
+%     end
+% end
+% 
+% %% Get frequency counts for distribution in space and time
+% min_reps = 2; % minimum number of times a repo can appear in the dataset
+% max_med_date_dist = 180; % maximum median distance between consecutive observations
+% nBins = 10;
+% 
+% % apply filter
+% date_rep_filter = find(rep_count_vec >= min_reps & date_diff_vec <= max_med_date_dist);
+% 
+% % get empircal distribution of counts
+% dateNumVecTrunc = dateNumVec(map_to);
+% posVecTrunc = dist_score_array(map_to);
+% [count_array,~,~,xID,yID] = histcounts2(dateNumVecTrunc,posVecTrunc,nBins);
+% 
+% % draw random sample weighted by empirical counts
+% linIndex = sub2ind(size(count_array),xID,yID);
+% rng(421);
+% region_index_list = randsample(date_rep_filter,1e4*length(date_rep_filter),true,1./count_array(linIndex(date_rep_filter)));
+% 
+% % enforce uniqueness
+% region_index_list_u = unique(region_index_list,'stable');
+% region_id_list_u = iu_rep(region_index_list_u);
+% 
+% % map back to original dimensions
+% % region_name_cell = {}; 
+% remapping_vec = [];
+% for i = 1:length(region_id_list_u)
+%     remapping_vec = [remapping_vec find(map_from==region_id_list_u(i))'];
+% %     region_name_cell = [region_name_cell{:} NameIndexCleanSorted(map_from==region_id_list_u(i))'];
+% end    
+% 
+% %% initialize structure
+% region_sampling_table = struct;
+% 
+% master_id_vec_orig = master_id_vec_sorted(remapping_vec);
+% [~,~,region_sampling_table.sample_order_vec] = unique(master_id_vec_orig,'stable');
+% 
+% region_sampling_table.region_name_clean = NameIndexCleanSorted(remapping_vec);
+% region_sampling_table.region_name_orig = NameIndexRawSorted(remapping_vec);
+% % region_sampling_table.date_num = dateNumVec(remapping_vec);
+% region_sampling_table.date_string = dateStringCell(remapping_vec);
+% 
+% 
+% region_sampling_table = struct2table(region_sampling_table);
+% 
+% % save
+% writetable(region_sampling_table,[OutDir 'region_sampling_list.csv']);
+% 
